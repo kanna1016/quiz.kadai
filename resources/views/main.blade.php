e33a47431c4527af7d61082fed495a8d613ac03a
kanna1016 <kanna1016kato@gmail.com>
2021-03-05 16:40:18 +0900

    main.blade.phpのコメント追加

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -13,17 +13,17 @@
   crossorigin="anonymous"></script>
   <script src="/js/main.js"></script>
 
-  @foreach ($questions as $a => $question)
+  @foreach ($questions as $a => $question)  <!--各問のループ-->
  
-    <h1 class="headline">第{{$a+1}}問</h1>
+    <h1 class="headline">第{{$a+1}}問</h1>  
 
-    <p>{{$question->text}}</p>
+    <p>{{$question->text}}</p>  <!--問題文を表示-->
 
     <table border="1" align="center">
 
-        @foreach($question->answers as $b => $answer)
+        @foreach($question->answers as $b => $answer) <!--選択肢のループ-->
           <tr>
-              <th>{{$b+1}}</th><th>{{$answer->text}}</th>
+              <th>{{$b+1}}</th><th>{{$answer->text}}</th> <!--選択肢の表示-->
           </tr>
 
         @endforeach
