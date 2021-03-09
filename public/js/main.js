b88ec34ade94f3763a3675f03a39f8e264e28979
kanna1016 <kanna1016kato@gmail.com>
2021-03-09 21:53:42 +0900

    完成

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,14 @@
 $(function(){
     let count = 0;
+    var tenn = 0;
+    var situation = 1;
     
     $('.next').click(function(){
         count++;
         show();
         $('.maru').hide();
         $('.batu').hide();
+        $('.next').hide();
     });
 
     show();
@@ -13,30 +16,48 @@ $(function(){
     function show(){
         $('.ques').hide();
         $('.ques.ques-'+count).show();
-        if (count == 9){
-            $('.next').hide();
-            $('.result').show();
-        }
-        else{$('.result').hide();
-         }
+        situation = 1;
     }   
 
+    $('.goResult').click(function(){
+        $('.ques.ques-'+count).hide();
+        $('.goResult').hide();
+        $('.maru').hide();
+        $('.batu').hide();
+        $('.result').show();
+        $('#tenn_area').text(tenn+'問正解');
+    });
+
     $('.clickable').click(function(){
-       $.ajax({
-           url:"/api/getAnswers/"+ $(this).data("answer_id"),
-           type:"get",
-       })
-       .then(function(data){
-                if (data.torf){
-                    $('.maru').show();
-                } else {
-                    $('.batu').show();
-                }
-            },
-            function(){
-                alert("error");
-            }
-       );
+        if (situation == 1){
+            situation = 2;
+            $.ajax({
+                url:"/api/getAnswers/"+ $(this).data("answer_id"),
+                type:"get",
+            })
+            .then(function(data){
+                        if (data.torf){
+                            $('.maru').show();
+                            tenn++;
+                            if (count == 9){
+                                $('.goResult').show();
+                            }
+                            else{$('.next').show();
+                            }
+                        } else {
+                            $('.batu').show();
+                            if (count == 9){
+                                $('.goResult').show();
+                            }
+                            else{$('.next').show();
+                            }
+                        }
+                    },
+                    function(){
+                        alert("error");
+                    }
+            );
+        }
     });
         
 });
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -11,14 +11,14 @@
   src="https://code.jquery.com/jquery-3.5.1.min.js"
   integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
   crossorigin="anonymous"></script>
-  <script src="/js/main.js"></script>
+  <script src="/js/main.js?upd=20210309"></script>
 
   <div style="position:relative">
-    <div class="maru" style="position:fixed;top:50px;left:50px;display:none">
-      <img src ="/images/maru.jpg">
+    <div class="maru" style="position:fixed;top:50%;left:50%; -webkit-transform : translate(-50%,-50%);transform : translate(-50%,-50%);display:none">
+      <img src ="/images/maru.png">
     </div>
-    <div class="batu" style="position:fixed;top:50px;left:50px;display:none">
-      <img src ="/images/batu.jpg">
+    <div class="batu" style="position:fixed;top:50%;left:50%; -webkit-transform : translate(-50%,-50%);transform : translate(-50%,-50%);display:none">
+      <img src ="/images/batu.png">
     </div>
   </div>
   
@@ -43,10 +43,16 @@
     </div>
     
   @endforeach
-  <button class="next">次へ</button>
-  <a href="/result">
-  <button class="result">結果</button>
-  </a>
+  <button class="next" style="display:none">次へ</button>
+  <button class="goResult" style="display:none">結果</button>
 
+  <div class=result style="display:none">
+    <h1 class="headline">結果</h1>
+
+    <p>{{$a+1}}問中</p>
+    <p id="tenn_area"></p>
+
+    <a href="/top"><button>トップへ</button></a>
+  </div>
 </body>
 </html>
